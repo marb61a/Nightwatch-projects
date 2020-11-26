e2b80d81d3e071f02fb74740c4147d06b754db70
Martin <marb61a@gmail.com>
2020-11-26 20:35:29 +0000

    updated notes and user actions file

--- nightwatch-tutorial/tests/user.actions.prompts.js
+++ nightwatch-tutorial/tests/user.actions.prompts.js
@@ -1,3 +1,13 @@
 module.exports = {
+    "Should perform key strokes": (client) => {
+        client
+            .url('https://ultimateqa.com/filling-out-forms/')
+            .setValue('#et_pb_contact_message_0', 'testing key strokes')
+            .keys(client.Keys.BACK_SPACE)
+            .keys(client.Keys.BACK_SPACE)
+            .keys(client.Keys.ENTER)
+            .keys(client.Keys.SUBTRACT)
+            .pause(5000);
 
+    }
 };
