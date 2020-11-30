1c3eef64cca78f81cb24d27a1fd42785e1ff74ed
Martin <marb61a@gmail.com>
2020-11-30 20:06:46 +0000

    Updated user actions test

--- nightwatch-tutorial/tests/user.actions.prompts.js
+++ nightwatch-tutorial/tests/user.actions.prompts.js
@@ -9,5 +9,33 @@ module.exports = {
             .keys(client.Keys.SUBTRACT)
             .pause(5000);
 
+    },
+
+    "Should perform right click": (client) => {
+        client
+            .mouseButtonClick('right')
+            .pause(3000);
+    },
+
+    "Should perform window actions": (client) => {
+        client
+            .openNewWindow('tab')
+            .fullscreenWindow()
+            .windowHandles((results) => {
+                const newWindow = results.value[1];
+                client.switchWindow(newWindow);
+            })
+            .pause(5000);
+    },
+
+    "Should interact with alerts": (client) => {
+        client
+            .url('http://demo.automationtesting.in/Alerts.html')
+            .click('#OKTab button')
+            .getAlertText((results) => {
+                console.log(results.value);
+            })
+            .acceptAlert();
     }
+
 };
