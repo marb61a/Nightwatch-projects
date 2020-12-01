51a80841c1313e6f62831c4c5a234e29dfb9fb97
Martin <marb61a@gmail.com>
2020-12-01 19:39:02 +0000

    Updated assert assertion test file

--- nightwatch-tutorial/tests/assert.assertion.js
+++ nightwatch-tutorial/tests/assert.assertion.js
@@ -2,7 +2,27 @@ module.exports = {
     "Expect Library demo tests" : (client) => {
         client
             .url('https://ultimateqa.com/filling-out-forms/')
-            .verify.not.visible('button[name="et_builder_submit_button"]:nth-child(1)')
+            .verify.visible('button[name="et_builder_submit_button"]:nth-child(1)')
             .assert.visible('button[name="et_builder_submit_button"]:nth-child(1)')
+    },
+
+    "Should assert form field value": (client) => {
+        client
+            .setValue('#et_pb_contact_name_0', 'Joe Bloggs');
+        client
+            .assert.valueContains('#et_pb_contact_name_0', 'Joe');
+    },
+
+    "Should assert current url": (client) => {
+        client.assert.urlEquals('https://ultimateqa.com/filling-out-forms/');
+    },
+
+    "Should assert title of the current page": (client) => {
+        client.verify.title('Filling Out Forms - Ultimate QA');
+    },
+
+    "Should assert attribute contains expected value": (client) => {
+        client.verify.attributeContains('#et_pb_contact_name_0', 'placeholder', "Name");
     }
+
 }
