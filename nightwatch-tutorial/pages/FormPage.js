e3a687c8f7a44822c10cb4319cbd2baf2792253b
Martin <marb61a@gmail.com>
2020-12-03 20:04:24 +0000

    Updated form page with elements and commands and form test to use page object

--- nightwatch-tutorial/pages/FormPage.js
+++ nightwatch-tutorial/pages/FormPage.js
@@ -9,21 +9,28 @@ const elements = {
     },
 
     form: '#et_pb_contact_form_0 form',
-    btnSubmit: `#et_pb_contact_form_0 form button`
+    btnSubmit: `#et_pb_contact_form_0 form button`,
+    lblLeftFormMessage: '#et_pb_contact_form_0 > div'
 };
 
 const commands = [
     {
         // Will enter the given name and message into the form fields
         enterNameAndMessage(name, message){
+            return this
+                .setValue('@leftFormName', name)
+                .setValue('@leftFormMessage', message)
+        },
 
+        submitLeftForm(){
+            return this.click('@btnSubmit')
         }
     }
 ]
 
 module.exports = {
-    elements: {},
-    commands: [],
+    elements: elements,
+    commands: commands,
     url: function(){
         return `${this.api.launchUrl}/filling-out-forms/`;
     }
--- nightwatch-tutorial/tests/forms.test.js
+++ nightwatch-tutorial/tests/forms.test.js
@@ -14,6 +14,17 @@ module.exports = {
             .expect.element('#et_pb_contact_form_0').text.to.be.equal('Form filled out successfully');
     },
 
+    "Should successfully fill out the left form using - Page Object": (client) => {
+        const FormPage = client.page.FormPage();
+
+        FormPage
+            .navigate()
+            .verify.title('Filling Out Forms - Ultimate QA')
+            .enterNameAndMessage('John Doe', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+            .submitLeftForm()
+            .verify.containsText('@lblLeftFormMessage', 'Form filled out successfully')
+    },
+
     "Should verify error message when the left form is not filled out and submitted": (client) => {
         client
             .refresh()
