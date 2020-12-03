bee4a740710fa83232815b61b8e65215adb1797a
Martin <marb61a@gmail.com>
2020-12-03 19:55:12 +0000

    Updated form page with elements and commands

--- nightwatch-tutorial/pages/FormPage.js
+++ nightwatch-tutorial/pages/FormPage.js
@@ -1,8 +1,31 @@
+const elements = {
+    // Shorthand element definition
+    leftFormName: "#et_pb_contact_name_0",
+
+    // Longhand/Full definition
+    leftFormMessage: {
+        selector: "#et_pb_contact_message_0",
+        locateStrategy: 'css selector'
+    },
+
+    form: '#et_pb_contact_form_0 form',
+    btnSubmit: `#et_pb_contact_form_0 form button`
+};
+
+const commands = [
+    {
+        // Will enter the given name and message into the form fields
+        enterNameAndMessage(name, message){
+
+        }
+    }
+]
+
 module.exports = {
     elements: {},
     commands: [],
     url: function(){
-
+        return `${this.api.launchUrl}/filling-out-forms/`;
     }
-    
+
 }
