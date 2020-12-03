17adc99ec03f088c488601b4a295f3934c10951f
Martin <marb61a@gmail.com>
2020-12-03 19:41:18 +0000

    Updated form test and added form page

---
+++ nightwatch-tutorial/pages/FormPage.js
@@ -0,0 +1,8 @@
+module.exports = {
+    elements: {},
+    commands: [],
+    url: function(){
+
+    }
+    
+}
--- nightwatch-tutorial/tests/forms.test.js
+++ nightwatch-tutorial/tests/forms.test.js
@@ -10,7 +10,15 @@ module.exports = {
             .verify.title('Filling Out Forms - Ultimate QA')
             .setValue('#et_pb_contact_name_0', 'John Doe')
             .setValue('#et_pb_contact_message_0', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry')
-            .submitForm('#et_pb_contact_form_0 form');
+            .submitForm('#et_pb_contact_form_0 form')
+            .expect.element('#et_pb_contact_form_0').text.to.be.equal('Form filled out successfully');
+    },
+
+    "Should verify error message when the left form is not filled out and submitted": (client) => {
+        client
+            .refresh()
+            .submitForm('#et_pb_contact_form_0 form')
+            .verify.containsText('#et_pb_contact_form_0 > div:nth-child(1)', 'Please, fill in the following fields:\nName\nMessage');
     },
 
     after: (client) => {
